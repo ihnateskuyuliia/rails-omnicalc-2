bc0633af8612449744717f060334891601cbee3d
ihnateskuyuliia <yuliiaihnatesku@uchicago.edu>
2024-10-28 19:01:25 +0000

    sub

--- app/controllers/subtraction_controller.rb
+++ app/controllers/subtraction_controller.rb
@@ -1,14 +1,17 @@
-class subtraction_controller < ApplicationController
-  def show_subtraction_form
+class SubtractionController < ApplicationController
+  def show_sub_form
     render({ :template => "subtraction_templates/subtraction_form" })
   end
 
   def subtract_these
-    @first_number = params.fetch("first_num").to_f
-    second_number = params.fetch("second_num").to_f
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    
+    @result = @second_num - @first_num
+    
+    #Result as a Float
+    @result_float = @result.to_f
 
-    @result =  @second_number - @first_number
-
-    render({ :template => "subtraction_templates/subtract_results" })
+    render({ :template => "subtraction_templates/sub_results" })
   end
 end
